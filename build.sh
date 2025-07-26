6c961ca9d50b1a63bd7f516b65920afb3d82a758
Joe D'Amore <joe@joedamore.me>
2025-07-26 18:35:29 -0400

    Add build script

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 set -ouex pipefail
+
+# Add tailscale repo
+dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+# Enable RPMFusion
+dnf5 -y install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+
+dnf -y install tailscale cockpit
+
+systemctl enable podman.socket
+systemctl enable cockpit.socket
+systemctl enable tailscaled
