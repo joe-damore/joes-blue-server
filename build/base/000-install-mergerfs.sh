2d6eaefbf1c787d4cd94ed1c28c620ac972c0e1e
Joe D'Amore <joe@joedamore.me>
2026-03-22 15:42:21 -0400

    Hardcode mergerfs architecture

--- build/base/000-install-mergerfs.sh
+++ build/base/000-install-mergerfs.sh
@@ -6,6 +6,7 @@ MERGERFS_REPO_URL="https://github.com/trapexit/mergerfs"
 MERGERFS_VERSION="2.41.1"
 MERGERFS_FEDORA_VERSION="43"
 
+# TODO Account for aarch64 systems.
 mkdir -p /tmp/mergerfs
-wget "$MERGERFS_REPO_URL/releases/download/$MERGERFS_VERSION/mergerfs-$MERGERFS_VERSION-1.fc$MERGERFS_FEDORA_VERSION.$BLUE_SERVER_IMAGE_ARCH.rpm" -O /tmp/mergerfs/mergerfs.rpm
+wget "$MERGERFS_REPO_URL/releases/download/$MERGERFS_VERSION/mergerfs-$MERGERFS_VERSION-1.fc$MERGERFS_FEDORA_VERSION.x86_64.rpm" -O /tmp/mergerfs/mergerfs.rpm
 dnf5 -y install /tmp/mergerfs/mergerfs.rpm
