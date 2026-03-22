dda45aeb11384d68ed6353442c13628acfcb49d1
Joe D'Amore <joe@joedamore.me>
2026-03-22 16:19:38 -0400

    Remove grub condition

--- rootfs/base/usr/libexec/rm-mount-for-root-from-etc-fstab.sh
+++ rootfs/base/usr/libexec/rm-mount-for-root-from-etc-fstab.sh
@@ -10,10 +10,12 @@ set -euo pipefail
 main() {
     # Used to condition execution of this unit at the systemd level
     local -r stamp_file="/var/lib/.rm_mount_for_root"
-
     local -r root_UUID="$(cat /proc/cmdline | grep -E -o 'root=UUID=.{36}' | cut -d '=' -f 2,3)"
-    if [[ $(grep -E '^'"${root_UUID}"'[[:space:]]/[[:space:]]' /etc/fstab) && \
-    $(findmnt / | grep composefs) && ! -L /boot/grub2/grub.cfg ]]; then
+
+    if [[ \
+        $(grep -E '^'"${root_UUID}"'[[:space:]]/[[:space:]]' /etc/fstab) && \
+        $(findmnt / | grep composefs)
+    ]]; then
         sed --in-place="-BACKUP" '/^'"${root_UUID}"'[[:space:]]\/[[:space:]]/d' /etc/fstab
         touch "${stamp_file}"
     else
